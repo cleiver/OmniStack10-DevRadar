cf752ee6fd7e109ae0e515ebbb6f114abc3fa809
cleiver <cleiver@gmail.com>
2020-04-05 22:08:49 -0300

    Install cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 const mongoose = require('mongoose')
 
 //------------------------------------------------------------------------------
@@ -14,6 +15,7 @@ mongoose.connect('mongodb://localhost:27017/devmap', {
 
 const routes = require('./routes')
 
+app.use(cors())
 app.use(express.json())
 app.use(routes)
 
